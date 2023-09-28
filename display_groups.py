4bb886b9fe3c581f504e1a7978af13372a4ea249
AkiMadie16 <madie86@gmail.com>
2023-09-28 18:17:04 +0930

    slicing a list to create a list

---
+++ display_groups.py
@@ -0,0 +1,23 @@
+players = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"]
+
+#Create 3 lists with 2 players each
+#Use slicing to create a list for Group 1
+g1 = players[0:2]
+
+#Use slicing to create a list for Group 2
+g2 = players[2:4]
+
+#Use slicing to create a list for Group 3
+g3 = players[4:6]
+
+print("Group 1:")
+#display the 1st group
+print(g1)
+
+print("Group 2:")
+#display the 2nd group
+print(g2)
+
+print("Group 3:")
+#display the 3rd group
+print(g3)
