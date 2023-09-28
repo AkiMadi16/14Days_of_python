139ec69e20fd6ccfa424da2c6556a15689270519
AkiMadie16 <madie86@gmail.com>
2023-09-28 18:24:43 +0930

    delivery cost display function

---
+++ delivery_cost_app.py
@@ -0,0 +1,11 @@
+#taking the weight as input
+weight = int(input())
+
+#complete the function
+def shipping_cost(weight):
+    cost = weight*5
+    print(cost)
+     
+
+#function call
+shipping_cost(weight)
