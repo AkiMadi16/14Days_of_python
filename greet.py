b78837fc4e57426e712c01de6931111a62d4c564
AkiMadie16 <madie86@gmail.com>
2023-09-28 18:12:08 +0930

    function introduction

---
+++ greet.py
@@ -0,0 +1,21 @@
+#function definition
+def greet(): 
+  print("Hello from a function")
+  print("Have a great day")
+
+#function call
+greet()
+# Hello from a function
+# Have a great day
+
+def personal_greet(name): 
+  print("Hello", name)
+  print("Have a great day")
+
+#function calls
+personal_greet("Sarah") 
+personal_greet("Henry")
+# Hello Sarah
+# Have a great day
+# Hello Henry
+# Have a great day
